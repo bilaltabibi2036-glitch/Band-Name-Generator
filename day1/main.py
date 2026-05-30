f3e961c1c3085a9dd3986bb2a8c27252454a491b
bilal36 <bilaltabibi2036@gmail.com>
2026-05-30 12:43:14 +0430

    Create main.py

---
+++ day1/main.py
@@ -0,0 +1,20 @@
+#                                                                             #
+# 1: CREATE A BAND NAME GENERATOR FOR YOUR PROGRAMMING CLASS. ASK THE USER    #
+# 2: FOR THE CITY THAT THEY GREW UP IN AND THE NAME OF A PET.                 #
+# 3: COMBINE THE NAME OF THEIR CITY AND PET AND SHOW THEM THEIR BAND NAME.    #
+
+print("welcome to the band name generator")
+
+# 2: ASK THE USER FOR THE CITY THAT THEY GREW UP IN
+
+city = input("which city you grew up in!\n")
+
+# 3: ASK THE USER FOR THE NAME OF A PET
+
+pet = input("what is your pet name!\n")
+
+# 4: COMBINE THE NAME OF THEIR CITY AND PET AND SHOW THEM THEIR BAND NAME
+
+print("your band name could be " + city + " " + pet)
+
+#                                                                              #
